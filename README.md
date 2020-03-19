31ce07a21a04cdc4330ced0e17d1a0d9b4cd9d22
xiaxuefeng <xiaxuefeng@jiangroom.com>
2020-03-19 22:21:37 +0800

    用Java实现斐波那契数列

---
+++ README.md
@@ -0,0 +1,5 @@
+源码中有中文时编译
+javac -encoding utf-8 x.java
+
+运行类名，而不是类文件名(即不加.class)
+java publicClassName
---
+++ Subject1.java
@@ -0,0 +1,68 @@
+/** 
+题目：有一对兔子，从出生后第3个月起每个月都生一对兔子，小兔子长到第三个月后每个月又生一对兔子，假如兔子都不死，问每个月的兔子对数为多少？
+*/
+
+// Scanner类用来获取用户输入
+import java.util.Scanner;
+
+
+public class Subject1
+{
+	public static void main(String[] args)
+	{
+		System.out.print("请输入你想知道的兔子数量的月份：");
+		Scanner scanner = new Scanner(System.in);
+		// 获取输入的整数 
+		int n = scanner.nextInt();
+		System.out.println(test(n));
+		System.out.println(fun(n));
+		scanner.close();
+	}
+
+	// 菜鸟写法
+	public static int test(int i){
+		int tot = 0;
+		int a = 0;
+		int b = 1;
+		for(int j = 0; j < i; j++){
+			tot = a + b;
+			a = b;
+			b = tot;
+		}
+		return a;
+	}
+	
+	// 大神写法
+	private static int fun(int n){
+		if(n ==1 || n ==2)
+			return 1;
+		else
+			return fun(n - 1) + fun(n - 2);
+	}
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
